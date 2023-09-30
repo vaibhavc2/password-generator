9054402cd00b557f91d8e8b9b8c700cefd1a89bd
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-09-30 09:27:14 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Password Generator
+
+This is a simple yet optimised web application of a Password Generator built in React. This project also uses Typescript and TailwindCSS. Best practices have been taken care of while developing this application.
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import PageNotFound from "./components/pages/PageNotFound";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
